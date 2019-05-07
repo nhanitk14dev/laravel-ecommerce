00f64133462415a1d96debfa0601d0fca8db7b1c
nhanitk14 <nhanitk14dev@gmail.com>
2019-05-08 00:44:10 +0700

    Upload code from source github --github.com/tomhapbia/laravel-ecommerce

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Release Notes
+
+## v1.6 (2019-09-08)
+
+> Upgrade to Laravel framework 5.6
+
+
+## v1.5.3 (2019-09-08)
+
+> Laravel framework 5.5
+
+### Fixed
+> Error promition
+> Duplicate order
+
+## v1.5.2 (2019-09-06)
+
+> Laravel framework 5.5
---
+++ public/js/extent.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(window).load(function() {
+$('.main-footer').html('<div class="pull-right hidden-xs"><strong>Version</strong>&nbsp;&nbsp; 1.0</div><strong>Develop by <a href="GiaiPhap247.Com">GiaiPhap247.Com</a></strong>');
+
+  });
+});
