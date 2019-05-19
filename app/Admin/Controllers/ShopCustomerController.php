68a620326dcbb8357b1fc9b75c76b6b67e758312
nhanitk14 <nhanitk14dev@gmail.com>
2019-05-19 18:40:24 +0700

    update some title menu admin & login get item cart from cookie

--- app/Admin/Controllers/ShopCustomerController.php
+++ app/Admin/Controllers/ShopCustomerController.php
@@ -4,6 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Admin\Extensions\ExcelExpoter;
 use App\Http\Controllers\Controller;
+use App\Models\ShopCategory;
 use App\User;
 use Encore\Admin\Controllers\ModelForm;
 use Encore\Admin\Facades\Admin;
@@ -12,148 +13,149 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class ShopCustomerController extends Controller
-{
-    use ModelForm;
+class ShopCustomerController extends Controller {
+  use ModelForm;
 
-    /**
-     * Index interface.
-     *
-     * @return Content
-     */
-    public function index()
-    {
-        return Admin::content(function (Content $content) {
+  /**
+   * Index interface.
+   *
+   * @return Content
+   */
+  public function index() {
+    return Admin::content(function (Content $content) {
 
-            // $content->description('description');
-            $action = \Request::input('action');
-            if ($action == 'report') {
-                $content->header('Thống kê khách hàng');
-                $content->body($this->report());
-            } else {
-                $content->header('Danh sách khách hàng');
-                // $content->description('description');
-                $content->body($this->grid());
-            }
+      // $content->description('description');
+      $action = \Request::input('action');
+      if ($action == 'report') {
+        $content->header('Thống kê khách hàng');
+        $content->body($this->report());
+      } else {
+        $content->header('Danh sách khách hàng');
+        // $content->description('description');
+        $content->body($this->grid());
+      }
 
-        });
-    }
+    });
+  }
 
-    /**
-     * Edit interface.
-     *
-     * @param $id
-     * @return Content
-     */
-    public function edit($id)
-    {
-        return Admin::content(function (Content $content) use ($id) {
+  /**
+   * Edit interface.
+   *
+   * @param $id
+   * @return Content
+   */
+  public function edit($id) {
+    return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('Chỉnh sửa');
-            // $content->description('description');
+      $content->header('Chỉnh sửa');
+      // $content->description('description');
 
-            $content->body($this->form()->edit($id));
-        });
-    }
+      $content->body($this->form()->edit($id));
+    });
+  }
 
-    /**
-     * Create interface.
-     *
-     * @return Content
-     */
-    public function create()
-    {
-        return Admin::content(function (Content $content) {
+  /**
+   * Create interface.
+   *
+   * @return Content
+   */
+  public function create() {
+    return Admin::content(function (Content $content) {
 
-            $content->header('Tạo mới');
-            // $content->description('description');
+      $content->header('Tạo mới');
+      // $content->description('description');
 
-            $content->body($this->form());
-        });
-    }
+      $content->body($this->form());
+    });
+  }
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        return Admin::grid(User::class, function (Grid $grid) {
+  /**
+   * Make a grid builder.
+   *
+   * @return Grid
+   */
+  protected function grid() {
+    return Admin::grid(User::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
-            $grid->email('Email')->sortable();
-            $grid->name('Name')->sortable();
+      $grid->id('ID')->sortable();
+      $grid->email('Email')->sortable();
+      $grid->name('Name')->sortable();
 
-            $grid->created_at('Ngày tạo');
-            $grid->updated_at('Lần cuối chỉnh sửa');
-            $grid->model()->orderBy('id', 'desc');
-            $grid->exporter(new ExcelExpoter('dataCustomer', 'Danh sach khach hang'));
-        });
-    }
+      $grid->created_at('Ngày tạo');
+      $grid->updated_at('Lần cuối chỉnh sửa');
+      $grid->model()->orderBy('id', 'desc');
+      $grid->exporter(new ExcelExpoter('dataCustomer', 'Danh sach khach hang'));
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        return Admin::form(User::class, function (Form $form) {
+      //Nhan off filter
+      $grid->filter(function ($filter) {
+        // Remove the default id filter
+        $filter->disableIdFilter();
+        // Add a column filter
+        $filter->like('name', 'Name');
+        $filter->like('email', 'Email');
+      });
+    });
+  }
 
-            $form->display('id', 'ID');
-            $form->text('name', 'Tên');
-            $form->email('email', 'Email');
-            $form->password('password', 'Password');
-            $form->text('address1', 'Address 1');
-            $form->text('address2', 'Address 2');
-            $form->text('phone', 'Phone');
-            $form->saving(function (Form $form) {
-                if ($form->password) {
-                    $form->password = bcrypt($form->password);
-                } else {
-                    $form->password = $form->model()->password;
-                }
-            });
-        });
-    }
+  /**
+   * Make a form builder.
+   *
+   * @return Form
+   */
+  protected function form() {
+    return Admin::form(User::class, function (Form $form) {
+
+      $form->display('id', 'ID');
+      $form->text('name', 'Tên');
+      $form->email('email', 'Email');
+      $form->password('password', 'Password');
+      $form->text('address1', 'Address 1');
+      $form->text('address2', 'Address 2');
+      $form->text('phone', 'Phone');
+      $form->saving(function (Form $form) {
+        if ($form->password) {
+          $form->password = bcrypt($form->password);
+        } else {
+          $form->password = $form->model()->password;
+        }
+      });
+    });
+  }
 
 /**
  * Report product
  * @return [type] [description]
  */
-    protected function report()
-    {
-        return Admin::grid(User::class, function (Grid $grid) {
+  protected function report() {
+    return Admin::grid(User::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
-            $grid->name('Tên khách hàng')->sortable();
-            $grid->email('Email')->sortable();
-            $grid->address2('Địa chỉ')->sortable();
-            $grid->orders('Tổng đơn hàng')->display(function ($orders) {
-                $total_order = count($orders);
-                return $total_order;
-            });
-            $grid->html('Tổng giá trị')->display(function () {
-                return number_format(User::find($this->id)->orders_amount());
-            });
-            $grid->created_at('Ngày đăng ký')->sortable();
-            $grid->model()->orderBy('id', 'desc');
-            // $grid->disableExport();
-            $grid->disableCreateButton();
-            $grid->disableRowSelector();
-            $grid->disableActions();
-            $grid->exporter(new ExcelExpoter('dataCustomer_report', 'Report khach hang'));
-        });
-    }
-    public function show($id)
-    {
-        return Admin::content(function (Content $content) use ($id) {
-            $content->header('');
-            $content->description('');
-            $content->body(Admin::show(ShopCategory::findOrFail($id), function (Show $show) {
-                $show->id('ID');
-            }));
-        });
-    }
+      $grid->id('ID')->sortable();
+      $grid->name('Tên khách hàng')->sortable();
+      $grid->email('Email')->sortable();
+      $grid->address2('Địa chỉ')->sortable();
+      $grid->orders('Tổng đơn hàng')->display(function ($orders) {
+        $total_order = count($orders);
+        return $total_order;
+      });
+      $grid->html('Tổng giá trị')->display(function () {
+        return number_format(User::find($this->id)->orders_amount());
+      });
+      $grid->created_at('Ngày đăng ký')->sortable();
+      $grid->model()->orderBy('id', 'desc');
+      // $grid->disableExport();
+      $grid->disableCreateButton();
+      $grid->disableRowSelector();
+      $grid->disableActions();
+      $grid->exporter(new ExcelExpoter('dataCustomer_report', 'Report khach hang'));
+    });
+  }
+  public function show($id) {
+    return Admin::content(function (Content $content) use ($id) {
+      $content->header('');
+      $content->description('');
+      $content->body(Admin::show(ShopCategory::findOrFail($id), function (Show $show) {
+        $show->id('ID');
+      }));
+    });
+  }
 }
--- app/Admin/Controllers/ShopOptionController.php
+++ app/Admin/Controllers/ShopOptionController.php
@@ -11,99 +11,92 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class ShopOptionController extends Controller
-{
-    use ModelForm;
+class ShopOptionController extends Controller {
+  use ModelForm;
 
-    /**
-     * Index interface.
-     *
-     * @return Content
-     */
-    public $arrType = ['1' => 'Radio', '2' => 'Select', '3' => 'Text'];
-    public function index()
-    {
-        return Admin::content(function (Content $content) {
+  /**
+   * Index interface.
+   *
+   * @return Content
+   */
+  public $arrType = ['1' => 'Radio', '2' => 'Select', '3' => 'Text'];
+  public function index() {
+    return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+      $content->header('Danh sách thuộc tính');
+      $content->description('Thuộc tính');
 
-            $content->body($this->grid());
-        });
-    }
+      $content->body($this->grid());
+    });
+  }
 
-    /**
-     * Edit interface.
-     *
-     * @param $id
-     * @return Content
-     */
-    public function edit($id)
-    {
-        return Admin::content(function (Content $content) use ($id) {
+  /**
+   * Edit interface.
+   *
+   * @param $id
+   * @return Content
+   */
+  public function edit($id) {
+    return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('Chỉnh sửa thuộc tính');
-            // $content->description('description');
+      $content->header('Chỉnh sửa thuộc tính');
+      // $content->description('description');
 
-            $content->body($this->form()->edit($id));
-        });
-    }
+      $content->body($this->form()->edit($id));
+    });
+  }
 
-    /**
-     * Create interface.
-     *
-     * @return Content
-     */
-    public function create()
-    {
-        return Admin::content(function (Content $content) {
+  /**
+   * Create interface.
+   *
+   * @return Content
+   */
+  public function create() {
+    return Admin::content(function (Content $content) {
 
-            $content->header('Thêm mới thuộc tính');
-            // $content->description('description');
+      $content->header('Thêm mới thuộc tính');
+      // $content->description('description');
 
-            $content->body($this->form());
-        });
-    }
+      $content->body($this->form());
+    });
+  }
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        return Admin::grid(ShopOption::class, function (Grid $grid) {
+  /**
+   * Make a grid builder.
+   *
+   * @return Grid
+   */
+  protected function grid() {
+    return Admin::grid(ShopOption::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
-            $grid->name('Tên thuộc tính')->sortable();
-            $grid->status('Hoạt động')->switch();
+      $grid->id('ID')->sortable();
+      $grid->name('Tên thuộc tính')->sortable();
+      $grid->status('Hoạt động')->switch();
 
-        });
-    }
+    });
+  }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        return Admin::form(ShopOption::class, function (Form $form) {
-            $form->text('name', 'Loại thuộc tính')->help('Ví dụ: Màu sắc, Size...');
-            $form->switch('status', 'Hoạt động');
-            $form->number('sort', 'Sắp xếp');
-            $form->select('type', 'Cách hiển thị')->options($this->arrType)->default('1');
-        });
-    }
+  /**
+   * Make a form builder.
+   *
+   * @return Form
+   */
+  protected function form() {
+    return Admin::form(ShopOption::class, function (Form $form) {
+      $form->text('name', 'Loại thuộc tính')->help('Ví dụ: Màu sắc, Size...');
+      $form->switch('status', 'Hoạt động');
+      $form->number('sort', 'Sắp xếp');
+      $form->select('type', 'Cách hiển thị')->options($this->arrType)->default('1');
+    });
+  }
 
-    public function show($id)
-    {
-        return Admin::content(function (Content $content) use ($id) {
-            $content->header('');
-            $content->description('');
-            $content->body(Admin::show(ShopOption::findOrFail($id), function (Show $show) {
-                $show->id('ID');
-            }));
-        });
-    }
+  public function show($id) {
+    return Admin::content(function (Content $content) use ($id) {
+      $content->header('');
+      $content->description('');
+      $content->body(Admin::show(ShopOption::findOrFail($id), function (Show $show) {
+        $show->id('ID');
+      }));
+    });
+  }
 }
